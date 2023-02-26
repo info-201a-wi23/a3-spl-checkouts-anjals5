68d55f7fb6476ed0f200023b25d57cf3572004d5
anjals5 <anjals5@uw.edu>
2023-02-26 13:37:15 -0800

    title trial 2

--- Chart2.R
+++ Chart2.R
@@ -24,10 +24,10 @@ pop_checkouts_per_year <- bt_popularity %>%
 #create line plot
 popular_plot <- ggplot(data = pop_checkouts_per_year) +
   geom_line(aes(x = CheckoutYear, y = total_check, color = Title)) +
-  labs(title = "Popularity of Booktok Books Over Time", 
-       x = "Year" , y = "Total Checkouts") +
-  scale_x_continuous(limits = c(2015, 2022)) 
-
+ scale_x_continuous(limits = c(2015, 2022)) +
+  labs(title = "Popularity of Booktok Books 
+       Over Time", 
+       x = "Year" , y = "Total Checkouts") 
 
 
   
