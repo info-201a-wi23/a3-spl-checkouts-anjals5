04bd50b1ae0981d8554f66ef250e9339525f2a2f
anjals5 <anjals5@uw.edu>
2023-02-25 20:12:36 -0800

    chart 1 title change

--- Chart1.R
+++ Chart1.R
@@ -2,6 +2,7 @@
 #Load Data from SPL
 spl_df <- read.csv("~/Downloads/2013-2023-5-Checkouts-SPL.csv", stringsAsFactors = FALSE)
 
+View(spl_df)
 # Load libraries
 library("dplyr")
 library("stringr")
@@ -9,7 +10,7 @@ library("ggplot2")
 
 # filtering for specific genres
 pop_genres_df <- spl_df %>% 
-  filter(Subjects %in% c("Young Adult Fiction", "Romance", "Nonfiction"))
+  filter(Subjects %in% c("Young Adult Fiction", "Romance", "Nonfiction", "Fantasy"))
 
 # sum of the total checkouts per year based on genre
 checkouts_per_year <- pop_genres_df %>% 
@@ -19,8 +20,8 @@ checkouts_per_year <- pop_genres_df %>%
 #create line plot
 genre_plot <- ggplot(data = checkouts_per_year) +
   geom_line(aes(x = CheckoutYear, y = total_checkouts, color = Subjects)) +
-  labs(title = "Romance and Young Adult Fiction
-       Checkouts Over the Years", 
+  labs(title = "Popular Genre Checkouts
+   Over the Years", 
        x = "Year" , y = "Total Checkouts") +
   scale_x_continuous(limits = c(2015, 2023)) 
 
